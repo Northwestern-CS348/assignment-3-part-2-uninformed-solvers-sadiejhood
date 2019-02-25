54caff1025809cde8772494d4195e4b2643485e5
Sadie Hood <sadiehood2020@u.northwestern.edu>
2019-02-24 21:42:54 -0600

    submission1

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -6,7 +6,7 @@ class TowerOfHanoiGame(GameMaster):
 
     def __init__(self):
         super().__init__()
-        
+
     def produceMovableQuery(self):
         """
         See overridden parent class method for more information.
@@ -34,6 +34,69 @@ class TowerOfHanoiGame(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### student code goes here
+        facts = self.kb.facts
+        # for f in facts:
+        #     print(f)
+        #
+        # print('\n\n\n')
+
+        elements_on = []
+        state = Statement(["on", "?x", "?y"])
+
+        for f in facts:
+            if (match(state, f.statement) ):
+                elements_on.append(f)
+
+        gameState = [[], [], []]
+
+        for el in elements_on:
+            bindings = match(state, el.statement)
+            # print(str(bindings) + " =?= ?X : disk1, ?Y : peg1")
+            # print(str(bindings) == "?X : disk1, ?Y : peg1")
+
+            if (str(bindings) == "?X : disk1, ?Y : peg1"):
+                gameState[0].append(1)
+            elif (str(bindings) == "?X : disk2, ?Y : peg1"):
+                gameState[0].append(2)
+            elif (str(bindings) == "?X : disk3, ?Y : peg1"):
+                gameState[0].append(3)
+            elif (str(bindings) == "?X : disk4, ?Y : peg1"):
+                gameState[0].append(4)
+            elif (str(bindings) == "?X : disk5, ?Y : peg1"):
+                gameState[0].append(5)
+            elif (str(bindings) == "?X : disk1, ?Y : peg2"):
+                gameState[1].append(1)
+            elif (str(bindings) == "?X : disk2, ?Y : peg2"):
+                gameState[1].append(2)
+            elif (str(bindings) == "?X : disk3, ?Y : peg2"):
+                gameState[1].append(3)
+            elif (str(bindings) == "?X : disk4, ?Y : peg2"):
+                gameState[1].append(4)
+            elif (str(bindings) == "?X : disk5, ?Y : peg2"):
+                gameState[1].append(5)
+            elif (str(bindings) == "?X : disk1, ?Y : peg3"):
+                gameState[2].append(1)
+            elif (str(bindings) == "?X : disk2, ?Y : peg3"):
+                gameState[2].append(2)
+            elif (str(bindings) == "?X : disk3, ?Y : peg3"):
+                gameState[2].append(3)
+            elif (str(bindings) == "?X : disk4, ?Y : peg3"):
+                gameState[2].append(4)
+            elif (str(bindings) == "?X : disk5, ?Y : peg3"):
+                gameState[2].append(5)
+
+        gameState[0].sort()
+        gameState[1].sort()
+        gameState[2].sort()
+
+        ## Create the string rep for the gameState
+        gameState[0] = tuple(gameState[0])
+        gameState[1] = tuple(gameState[1])
+        gameState[2] = tuple(gameState[2])
+        return_string = tuple(gameState)
+
+        return return_string
+
         pass
 
     def makeMove(self, movable_statement):
@@ -52,8 +115,155 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
-        pass
+
+        retracted = []
+        added = []
+        facts = self.kb.facts
+        print(self.getGameState())
+        for f in facts:
+            if(f.statement.predicate == "movable"):
+                print(f)
+        print("\n")
+
+        # fs = self.getMovables()
+        #
+        # self.getGameState()
+        # for f in fs:
+        #     print(f)
+
+        new_location = movable_statement.terms[2]
+        object = movable_statement.terms[0]
+        old_location = movable_statement.terms[1]
+
+        old_stack = self.kb.kb_ask(Fact(Statement(["onTopOf", object, "?X"])))
+        new_is_empty = self.kb.kb_ask(Fact(Statement(["empty", new_location])))
+        new_stack = self.kb.kb_ask(Fact(Statement(["on", "?x", new_location])))
+
+        # print(old_stack.list_of_bindings[0][0].bindings_dict["?X"])
+        # print("OLD")
+        # print(old_stack)
+        # print("New")
+        # print(new_stack)
+
+        # OPTION 1: Move from stack to empty
+        if (old_stack and new_is_empty):
+            # change top, add new top, retract ontopof, get rid of empty, change ons
+            self.kb.kb_retract(Fact(Statement(["empty", new_location])))
+            self.kb.kb_retract(Fact(Statement(["top", object, old_location])))
+            self.kb.kb_retract(Fact(Statement(["onTopOf", object, old_stack.list_of_bindings[0][0].bindings_dict["?X"]])))
+            self.kb.kb_retract(Fact(Statement(["on", object, old_location])))
+
+            self.kb.kb_assert(Fact(Statement(["on", object, new_location])))
+            self.kb.kb_assert(Fact(Statement(["top", old_stack.list_of_bindings[0][0].bindings_dict["?X"], old_location])))
+            self.kb.kb_assert(Fact(Statement(["top", object, new_location])))
+
+        # OPTION 2: Move from stack to stack
+        elif (old_stack and new_stack):
+
+            self.kb.kb_retract(Fact(Statement(["top", object, old_location])))
+            self.kb.kb_retract(Fact(Statement(["onTopOf", object, old_stack.list_of_bindings[0][0].bindings_dict["?X"]])))
+            self.kb.kb_retract(Fact(Statement(["top", new_stack.list_of_bindings[0][0].bindings_dict["?x"], new_location])))
+            self.kb.kb_retract(Fact(Statement(["on", object, old_location])))
+
+            self.kb.kb_assert(Fact(Statement(["on", object, new_location])))
+            self.kb.kb_assert(Fact(Statement(["onTopOf", object, new_stack.list_of_bindings[0][0].bindings_dict["?x"]])))
+            self.kb.kb_assert(Fact(Statement(["top", old_stack.list_of_bindings[0][0].bindings_dict["?X"], old_location])))
+            self.kb.kb_assert(Fact(Statement(["top", object, new_location])))
+
+        # OPTION 3: Move from empty to stack
+        elif (new_stack):
+            self.kb.kb_retract(Fact(Statement(["top", object, old_location])))
+            self.kb.kb_retract(Fact(Statement(["top", new_stack.list_of_bindings[0][0].bindings_dict["?x"], new_location])))
+            self.kb.kb_retract(Fact(Statement(["on", object, old_location])))
+
+            self.kb.kb_assert(Fact(Statement(["onTopOf", object, new_stack.list_of_bindings[0][0].bindings_dict["?x"]])))
+            self.kb.kb_assert(Fact(Statement(["top", object, new_location])))
+            self.kb.kb_assert(Fact(Statement(["empty", old_location])))
+            self.kb.kb_assert(Fact(Statement(["on", object, new_location])))
+
+
+        # OPTION 4: Move from empty to empty
+        else:
+            self.kb.kb_retract(Fact(Statement(["top", object, old_location])))
+            self.kb.kb_retract(Fact(Statement(["empty", new_location])))
+            self.kb.kb_retract(Fact(Statement(["on", object, old_location])))
+
+            self.kb.kb_assert(Fact(Statement(["top", object, new_location])))
+            self.kb.kb_assert(Fact(Statement(["empty", old_location])))
+            self.kb.kb_assert(Fact(Statement(["on", object, new_location])))
+
+        self.kb.kb_retract(Fact(movable_statement))
+        #
+        # # for f in facts:
+        # #     print(f)
+        # # print('\n\n\n')
+        #
+        # new_location = movable_statement.terms[2]
+        # object = movable_statement.terms[0]
+        # old_location = movable_statement.terms[1]
+        #
+        # statement_new = Statement(["on", object, new_location] )
+        # statement_old = Statement(["on", object, old_location] )
+        #
+        # # Add new ons
+        # if (Fact(Statement(["on", "disk2", old_location])) in facts):
+        #     added.append(Fact(Statement(["top", "disk2", old_location])))
+        # elif (Fact(Statement(["on", "disk3", old_location])) in facts):
+        #     added.append(Fact(Statement(["top", "disk3", old_location])))
+        #
+        # # Retract old empty
+        # if (Fact(Statement(["empty", new_location])) in facts):
+        #     # Must get rid of previous top
+        #     for f in facts:
+        #         if (f.statement.predicate == "top" and f.statement.terms[1] == new_location):
+        #             retracted.append(f)
+        #             break
+        #
+        # new_fact = Fact( statement_new )
+        # old_fact = Fact( statement_old )
+        #
+        # retracted.append(old_fact)
+        # added.append(new_fact)
+        # # self.kb.kb_assert(new_fact)
+        #
+        #
+        # # We know moved disk was and still is a top, simply need to switch top type
+        # retracted.append(Fact(Statement(["top", object, old_location])))
+        # added.append(Fact(Statement(["top", object, new_location])))
+        #
+        # # retract old onTopOf
+        # if (Fact(Statement(["onTopOf", "disk2", object])) in facts):
+        #     retracted.append(Fact(Statement(["onTopOf", "disk2", object])))
+        # elif (Fact(Statement(["onTopOf", "disk3", object])) in facts):
+        #     retracted.append(Fact(Statement(["onTopOf", "disk3", object])))
+        #
+        # if (not Fact(Statement(["empty", new_location]))):
+        #     # We need to get rid of the top on it and add an onTopOf
+        #     if (Fact(Statement(["on", "disk2", new_location])) in facts):
+        #         retract.append(Fact(Statement(["top", "disk2", new_location])))
+        #         added.append(Fact(Statement(["onTopOf", object, "disk2"])))
+        #     elif (Fact(Statement(["on", "disk3", new_location])) in facts):
+        #         retract.append(Fact(Statement(["top", "disk3", new_location])))
+        #         added.append(Fact(Statement(["onTopOf", object, "disk3"])))
+        #
+        # for a in added:
+        #     if ((a not in facts)):
+        #         # print("Added")
+        #         # print(a)
+        #         self.kb.kb_assert(a)
+        #
+        # for r in retracted:
+        #     # print("Retracted")
+        #     # print(r)
+        #     self.kb.kb_retract(r)
+        #
+        # # Assert new empties
+        # states = self.getGameState()
+        # for i, s in enumerate(states):
+        #     if (s == ()):
+        #         peg = "peg" + str(i + 1)
+        #         self.kb.kb_assert(Fact(Statement(["empty", peg])))
+
 
     def reverseMove(self, movable_statement):
         """
@@ -100,6 +310,18 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
+        tupl = [['', '', ''], ['', '', ''], ['', '', '']]
+        facts = self.kb.facts
+
+        for f in facts:
+            if f.statement.predicate == "coordinate":
+                if (not str(f.statement.terms[0]) == "empty"):
+                    tupl[int(str(f.statement.terms[2])[3]) - 1][int(str(f.statement.terms[1])[3]) - 1] = int(str(f.statement.terms[0])[4])
+                else:
+                    tupl[int(str(f.statement.terms[2])[3]) - 1][int(str(f.statement.terms[1])[3]) - 1] = -1
+        t = tuple([tuple(tupl[0]), tuple(tupl[1]), tuple(tupl[2])])
+
+        return t
         pass
 
     def makeMove(self, movable_statement):
@@ -119,6 +341,25 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
+
+        object = movable_statement.terms[0]
+        old_x_pos = movable_statement.terms[1]
+        old_y_pos = movable_statement.terms[2]
+        new_x_pos = movable_statement.terms[3]
+        new_y_pos = movable_statement.terms[4]
+
+        new_fact = Fact(Statement(["coordinate", object, new_x_pos, new_y_pos]))
+        old_fact = Fact(Statement(["coordinate", object, old_x_pos, old_y_pos]))
+
+        old_empty = Fact(Statement(["coordinate", "empty", new_x_pos, new_y_pos]))
+        new_empty = Fact(Statement(["coordinate", "empty", old_x_pos, old_y_pos]))
+
+        self.kb.kb_retract(old_fact)
+        self.kb.kb_retract(old_empty)
+
+        self.kb.kb_assert(new_fact)
+        self.kb.kb_assert(new_empty)
+
         pass
 
     def reverseMove(self, movable_statement):
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,9 +1,29 @@
 
 from solver import *
+from logical_classes import *
+import queue
+
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.visited_states = {
+            ((1, 2, 3), (), ()): False,
+            ((2, 3), (1,), ()): False,
+            ((2, 3), (), (1,)): False,
+            ((3,), (1,), (2,)): False,
+            ((3,), (2,), (1,)): False,
+            ((3,), (1, 2), ()): False,
+            ((3,), (), (1,2)): False,
+            ((), (1, 2), (3)): False,
+            ((3,), (1, 2), ()): False,
+            ((), (3,), (1,2)): False,
+            ((3,), (), (1,2)): False,
+            ((1, 2), (), (3,)): False,
+            ((1, 2), (3,), ()): False,
+            ((), (1, 2, 3), ()): False,
+            ((), (), (1, 2, 3)): False
+        }
 
     def solveOneStep(self):
         """
@@ -19,7 +39,107 @@ class SolverDFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+
+        if (self.currentState.state == self.victoryCondition):
+            return True
+
+        if (self.currentState.children == []):
+            moveFromHere = self.gm.getMovables()
+            for m in moveFromHere:
+                self.gm.makeMove(m)
+                new_state = GameState(self.gm.getGameState(), self.currentState.depth+1, m)
+                new_state.requiredMovable = m
+                self.gm.reverseMove(m)
+                new_state.parent = self.currentState
+                self.visited[new_state] = False
+
+                # print(new_state.state)
+                if ( (new_state.state in self.visited_states) and self.visited_states[new_state.state] == False):
+                    self.currentState.children.append(new_state)
+                    # print("New State " + str(new_state.state))
+                    # print("Depth " + str(self.currentState.depth + 1))
+                    # print("Parent " + str(new_state.parent.requiredMovable))
+                    # print('\n')
+                elif (not self.currentState.parent):
+                    self.currentState.children.append(new_state)
+                    # print("New State " + str(new_state.state))
+                    # print("Depth " + str(self.currentState.depth + 1))
+                    # print("Parent " + str(new_state.parent.requiredMovable))
+                    # print('\n')
+
+        if (self.currentState.nextChildToVisit + 1 > len(self.currentState.children)):
+            # print(self.currentState.state)
+            # print(self.currentState.requiredMovable)
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+            return False
+
+        if ((len(self.currentState.children) > 0) and not self.visited[self.currentState.children[self.currentState.nextChildToVisit]] == True):
+            # print("Wow")
+            self.gm.makeMove(self.currentState.children[self.currentState.nextChildToVisit].requiredMovable)
+            self.visited[self.currentState.children[self.currentState.nextChildToVisit - 1]] = True
+            self.visited_states[self.currentState.state] = True
+            self.currentState.nextChildToVisit += 1
+            self.currentState = self.currentState.children[self.currentState.nextChildToVisit - 1]
+            # print(self.currentState.requiredMovable)
+            return False
+        else:
+            # print("wow2")
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+            return False
+
+
+        # print(self.currentState.requiredMovable)
+        # self.gm.reverseMove(self.currentState.requiredMovable)
+        # self.currentState = self.currentState.parent
+        # return False
+
+
+
+        # self.gm.kb_reverseMove(self.currentState.requiredMovable)
+        # return
+
+        # # stack - can just use a list
+        # self.currentState.children = []
+        # # print(self.gm.getMovables())
+        # if (self.gm.getMovables()):
+        #     for el in self.gm.getMovables():
+        #         self.currentState.children.append(el)
+        #
+        # for el in self.currentState.children:
+        #     if (not el in self.gm.getMovables()):
+        #         del el
+
+
+        # print(self.currentState.children[1])
+
+        # if( len(self.currentState.children) == 0 ):
+        #     return False
+        #
+        # while (len(self.currentState.children) != 0):
+        #     new_state = GameState(self.gm.getGameState(), self.currentState.depth+1, self.currentState.children[0])
+        #     new_state.parent = self.currentState
+        #     new_state.children = self.currentState.children
+        #
+        #     if (type(self.currentState.children) is list):
+        #         new_move = self.currentState.children[0]
+        #     else:
+        #         new_move = self.currentState.children
+        #
+        #     self.currentState.children.pop(0)
+        #     new_state.children = self.currentState.children
+        #     # print(new_move)
+        #
+        #     self.gm.makeMove(new_move)
+        #     new_state.state = self.gm.getGameState()
+        #
+        #     if (self.gm.isWon()):
+        #         return True
+        #     else:
+        #         return
+
+
 
 
 class SolverBFS(UninformedSolver):
@@ -40,4 +160,46 @@ class SolverBFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+        # stack - can just use a list
+        self.currentState.children = []
+        # print(self.gm.getMovables())
+        if (self.gm.getMovables()):
+            for el in self.gm.getMovables():
+                self.currentState.children.append(el)
+
+        for el in self.currentState.children:
+            if (not el in self.gm.getMovables()):
+                del el
+
+
+        # print(self.currentState.children[1])
+
+        if( len(self.currentState.children) == 0 ):
+            return False
+
+        while (len(self.currentState.children) != 0):
+            new_state = GameState(self.gm.getGameState(), self.currentState.depth+1, self.currentState.children[0])
+            new_state.parent = self.currentState
+            new_state.children = self.currentState.children
+
+            # print(self.visited[new_state])
+
+            if (not self.visited[new_state]):
+                self.currentState.children.pop(0)
+            else:
+                if (type(self.currentState.children) is list):
+                    new_move = self.currentState.children[0]
+                else:
+                    new_move = self.currentState.children
+
+                self.currentState.children.pop(0)
+                new_state.children = self.currentState.children
+                # print(new_move)
+
+                self.gm.makeMove(new_move)
+                new_state.state = self.gm.getGameState()
+
+                if (self.gm.isWon()):
+                    return True
+                else:
+                    return
